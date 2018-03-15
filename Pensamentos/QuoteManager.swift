b18699a787cf47e847bf37d28985f4041e4d46d0
Usuário Convidado <Guest@pa06micro02.local>
2018-03-14 23:00:47 -0300

    Finalizando app

---
+++ Pensamentos/QuoteManager.swift
@@ -0,0 +1,30 @@
+//
+//  QuoteManager.swift
+//  Pensamentos
+//
+//  Created by Usuário Convidado on 14/03/18.
+//  Copyright © 2018 Fiap. All rights reserved.
+//
+
+import Foundation
+
+class QuotesManager {
+    
+    var quotes: [Quote]
+    static let shared: QuotesManager = QuotesManager() //singleton ios
+    
+    private init() {
+        let fileURL = Bundle.main.url(forResource: "quotes", withExtension: "json")!
+        let jsonData = try! Data(contentsOf: fileURL)
+        quotes = try! JSONDecoder().decode([Quote].self, from: jsonData)
+    }
+    
+    func getRandomQuote() -> Quote {
+        let index = Int(arc4random_uniform(UInt32(quotes.count)))
+        return quotes[index]
+    }
+    
+    func save(quote: Quote) {
+        quotes.append(quote)
+    }
+}
